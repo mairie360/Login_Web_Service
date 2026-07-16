77bcc01495148a4649a3d22a83e54e9337d54293
CarolinHugo <hugo.carolin@epitech.eu>
2026-07-16 05:55:57 +0400

    fix: guarantee project redirect after login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Login from "../components/Login";
 
+const DEFAULT_PROJECT_FRONT_URL = "http://localhost:5001/";
+
 export default function Home() {
-  return <Login redirectUrl={process.env.PROJECT_FRONT_URL} />;
+  return (
+    <Login
+      redirectUrl={process.env.PROJECT_FRONT_URL || DEFAULT_PROJECT_FRONT_URL}
+    />
+  );
 }
